c6c2d1e0e58fd55683b9ba7b8e2338559ee46197
Gabriel Souza Leite <gabriel.s.leite05@gmail.com>
2023-11-30 10:02:46 -0300

    Corrigindo bug na busca do usuário :bug:

--- src/utils/extrat-query-params.js
+++ src/utils/extrat-query-params.js
@@ -2,7 +2,7 @@ export function extractQueryParams(query) {
   return query
     .substr(1)
     .split("&")
-    .reduce(() => {
+    .reduce((queryParams, param) => {
       const [key, value] = param.split("=");
 
       queryParams[key] = value;
